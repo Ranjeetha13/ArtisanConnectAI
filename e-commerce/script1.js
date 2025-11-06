867b418330c24550fc18029b309b661e6074b07e
Ranjeetha13 <ranjeethamohan2006@gmail.com>
2025-11-06 13:04:48 +0530

    Delete e-commerce directory

--- e-commerce/script1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const ctaButton = document.querySelector(".cta-button");
-
-  // CTA button click handler
-  ctaButton.addEventListener("click", () => {
-    console.log("Explore Now clicked! Redirecting to shop...");
-    window.location.href = "/shop";
-  });
-});
